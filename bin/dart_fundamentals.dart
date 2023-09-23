e2ef3be8dec4bf3724087500c1bf9da48a6a0678
Lucas Magnago de Oliveira <lucas.magnago.02@gmail.com>
2023-09-23 09:22:39 -0300

    Laços de repetição

--- bin/dart_fundamentals.dart
+++ bin/dart_fundamentals.dart
@@ -1,50 +1,79 @@
+import 'dart:ffi';
 import 'dart:io';
 
 import 'package:dart_fundamentals/dart_fundamentals.dart' as dart_fundamentals;
 
 void main(List<String> arguments) {
-  print("---IF---");
-  int number1 = 1;
+  // print("---IF---");
+  // int number1 = 1;
 
-  if (number1 > 0) {
-    print("O número é positivo");
-  } else if (number1 < 0) {
-    print('O número é negativo');
-  } else {
-    print('O número é zero');
+  // if (number1 > 0) {
+  //   print("O número é positivo");
+  // } else if (number1 < 0) {
+  //   print('O número é negativo');
+  // } else {
+  //   print('O número é zero');
+  // }
+
+  // print("---Lendo dados do console---");
+  // print('Digite algo: ');
+  // var input = stdin.readLineSync();
+  // print('Você digitou: $input');
+
+  // print('---SWITCH---');
+  // double number2 = 10;
+  // double number3 = 27;
+
+  // print('Digite a operação matemática (+, -, *, /): ');
+  // var operator = stdin.readLineSync();
+
+  // switch (operator) {
+  //   case "+":
+  //     print('Resultado: ${number2 + number3}');
+  //     break;
+  //   case "-":
+  //     print('Resultado: ${number2 - number3}');
+  //     break;
+  //   case "*":
+  //     print('Resultado: ${number2 * number3}');
+  //     break;
+  //   case "/":
+  //     print('Resultado: ${number2 / number3}');
+  //     break;
+  //   default:
+  //     print("Operação inválida!");
+  // }
+
+  // print('---FOR---');
+  // for (var i = 0; i < 99; i++) {
+  //   print(i);
+  // }
+
+  print('---FOR IN---');
+  List<String> lstNames = ['Lucas', 'Matheus', 'João'];
+  for (String name in lstNames) {
+    print(name);
   }
 
-  print("---Lendo dados do console---");
-  print('Digite algo: ');
-  var input = stdin.readLineSync();
-  print('Você digitou: $input');
+  print('---FOREACH---');
+  lstNames.forEach((element) {
+    print(element.toUpperCase());
+  });
 
-  print('---SWITCH---');
-  double number2 = 10;
-  double number3 = 27;
+  print('---WHILE---');
+  int cont = 0;
 
-  print('Digite a operação matemática (+, -, *, /): ');
-  var operator = stdin.readLineSync();
-
-  switch (operator) {
-    case "+":
-      print('Resultado: ${number2 + number3}');
-      break;
-    case "-":
-      print('Resultado: ${number2 - number3}');
-      break;
-    case "*":
-      print('Resultado: ${number2 * number3}');
-      break;
-    case "/":
-      print('Resultado: ${number2 / number3}');
-      break;
-    default:
-      print("Operação inválida!");
+  while (cont < 99) {
+    print(cont);
+    cont++;
   }
 
-  print('---FOR---');
-  for (var i = 0; i < 99; i++) {
-    print(i);
-  }
+  print('---DO WHILE---');
+  int cont2 = 0;
+
+  do {
+    print(cont2);
+    cont2++;
+    print(cont2);
+  } while (cont2 < 9);
 }
